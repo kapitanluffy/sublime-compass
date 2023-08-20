fd37708a59b8681e337be8732a1e66026377c316
kapitanluffy <kapitanluffy@gmail.com>
2023-08-21 00:10:22 +0800

    Update on show command
    - Don't show list if the viewstack is empty
    - Don't show list if the viewstack's group has no views
    - Add showing line preview for more context
    - Each item should now have a "kind" depending on the sheet's status
    - Add hacky tags for better filtering; current available tags are:
      files, empty, dirty, search, primary, clones
    - Invalid view stacks will now be skipped.
      Invalid view stacks does not have at least one valid view
        on show command

--- show_command.py
+++ show_command.py
@@ -1,3 +1,4 @@
+from typing import List
 import sublime
 import sublime_plugin
 from .utils import plugin_settings, plugin_state
@@ -5,7 +6,7 @@ from .view_stack import ViewStack
 from .stack_manager import StackManager
 import copy
 import os
-from typing import List
+import re
 
 class RecentlyUsedExtendedShowCommand(sublime_plugin.WindowCommand):
     ignore_highlight = False
@@ -19,12 +20,11 @@ class RecentlyUsedExtendedShowCommand(sublime_plugin.WindowCommand):
         group = self.window.active_group()
         stack = StackManager.get(self.window, group)
 
-        if self.window.views_in_group(0).__len__() <= 0:
+        if stack.sheet_total() == 0:
             return
 
-        if stack.sheet_total() == 0:
-            print("new stack! building stack! #2")
-            self.window.run_command("recently_used_extended_build_stack")
+        if self.window.views_in_group(group).__len__() <= 0:
+            return
 
         items = []
         current_view = self.window.active_view_in_group(group)
@@ -46,24 +46,84 @@ class RecentlyUsedExtendedShowCommand(sublime_plugin.WindowCommand):
         for index,sheets in enumerate(stack_sheets):
             names = []
             annotation = "#%s" % (index + 1)
+            preview = ""
+            kind = sublime.KIND_FUNCTION
+            tags = set()
 
             for sheet in sheets:
                 view = sheet.view()
+
+                if view is None or view.is_valid() is False:
+                    continue
+
+                name = "Untitled #%s" % sheet.id()
                 file_name = sheet.file_name()
 
                 if file_name:
                     name = os.path.basename(file_name)
-                elif view is not None:
+                elif view.name() != "":
                     name = view.name()
-                else:
-                    name = "Untitled #%s" % sheet.id()
 
                 names.append(name)
 
+                # Get preview of current line
+                if preview == "":
+                    visible_lines = view.lines(view.visible_region())
+                    view_selection = view.line(view.sel()[0].a)
+                    preview = view.substr(view_selection).strip()
+
+                    if preview.__len__() <= 0:
+                        preview_lines = self.get_visible_lines(view, visible_lines, view_selection)
+                        preview = preview_lines[0].lstrip() if preview_lines.__len__() > 0 and preview_lines[0].lstrip() != "" else preview
+
+                    # @todo for groups, show the preview for the currently active in that group
+                    preview = """<tt style='color:red'>%s</tt>""" % sublime.html.escape(preview)
+
+                # Set item type
+                # Normal sheet
+                if view.file_name() is not None and view.element() is None:
+                    tags.add("#files")
+
+                # Empty sheet
+                if view.file_name() is None and view.element() is None:
+                    tags.add("#empty")
+                    kind = (sublime.KindId.SNIPPET, "E", "Snippet")
+
+                # Dirty sheets
+                if  view.file_name() is not None and view.is_dirty():
+                    tags.add("#dirty")
+                    kind = (sublime.KindId.VARIABLE, "d", "Type")
+
+                # Find results
+                if view.element() == "find_in_files:output":
+                    tags.add("#search")
+                    kind = (sublime.KindId.NAMESPACE, "?", "Namespace")
+
+                # Sheets with clones
+                if view.is_primary() is True and view.clones().__len__() > 0:
+                    tags.add("#primary")
+                    kind = (sublime.KindId.KEYWORD, "P", "Keyword")
+
+                # Sheet clones
+                if view.is_primary() is False and view.clones().__len__() > 0:
+                    tags.add("#clones")
+                    kind = (sublime.KindId.KEYWORD, "c", "Keyword")
+
+            if names.__len__() <= 0:
+                continue
+
             trigger = ', '.join(names)
-            item = sublime.QuickPanelItem(trigger=trigger, annotation=annotation, kind=sublime.KIND_FUNCTION)
+
+            # @todo hacky tags for filter
+            spaces = [' '] * 10
+            spaces = ''.join(spaces)
+            trigger = "%s%s%s" % (trigger, spaces, ' '.join(tags))
+            annotation = ' '.join(tags)
+
+            item = sublime.QuickPanelItem(trigger=trigger, annotation=annotation, kind=kind, details=preview)
             items.append(item)
 
+        # @note planning to include existing files in the folder
         # folders = self.window.folders()
         # for folder in folders:
         #     for root, dirs, files in os.walk(folder):
@@ -80,6 +140,48 @@ class RecentlyUsedExtendedShowCommand(sublime_plugin.WindowCommand):
             on_select=lambda index, event=None: self.on_done(index, items, stack, initial_selection, event),
             on_highlight=lambda index: self.on_highlight(index, items, stack, initial_selection)
         )
+
+    def replace_spaces_with_spaces(self, text):
+        match = re.match(r'^(\s+)', text)
+        if match:
+            spaces = match.group(1)
+            return re.sub(r'^\s+', ' ' * len(spaces), text)
+        else:
+            return text
+
+    # Gets the visible lines surrounding the current line
+    def get_visible_lines(self, view: sublime.View, visible_lines, view_selection):
+        fallback_line_string = None
+        line_strings: List[str] = []
+        MAX_SURROUND_LINE_THRESHOLD = 1
+
+        for line_index, line in enumerate(visible_lines):
+            line_string = view.substr(line).rstrip()
+
+            if fallback_line_string is None and line_string != "":
+                fallback_line_string = line_string
+
+            if line != view_selection:
+                continue
+
+            real_line_threshold = MAX_SURROUND_LINE_THRESHOLD + 1
+            lines = list(range(line_index-1, line_index-real_line_threshold, -1)) + [line_index] + list(range(line_index+1, line_index+real_line_threshold, 1))
+
+            for preview_line in lines:
+                if preview_line < 0 or preview_line >= len(visible_lines):
+                    continue
+
+                line_string = view.substr(visible_lines[preview_line])
+                # @note does not work
+                # line_string = self.replace_spaces_with_spaces(line_string)
+
+                if line_string != "":
+                    line_strings.append(line_string)
+
+            if line_strings.__len__() > 0:
+                return line_strings
+
+        return [fallback_line_string or ""]
 
     def on_highlight(self, index, items, stack: ViewStack, selection):
         if index == -1:
