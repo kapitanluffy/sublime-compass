0a452334416f863a9b6c0eb44cb3639bffbcb06a
kapitanluffy <kapitanluffy@gmail.com>
2023-09-01 10:35:51 +0800

    Apply update
    Added a "post list" of quick panel items that would help instead of
    cramming everything in one item
    
    - Fix quick panel not opening on other windows
    - Refactor functions out of the show command class
    - Try another way of listing tagged items
    - Add relative file path to tagged items to enable us to
      search with file path

--- show_command.py
+++ show_command.py
@@ -7,15 +7,131 @@ from .stack_manager import StackManager
 import copy
 import os
 import re
+from pathlib import Path
+
+def generate_view_meta(view: sublime.View):
+    tags = set()
+    kind = sublime.KIND_FUNCTION
+
+    # Set item type
+    # Normal sheet
+    if view.file_name() is not None and view.element() is None:
+        tags.add("#files")
+
+    # Empty sheet
+    if view.file_name() is None and view.element() is None:
+        tags.add("#empty")
+        kind = (sublime.KindId.SNIPPET, "E", "Snippet")
+
+    # Dirty sheets
+    if  view.file_name() is not None and view.is_dirty():
+        tags.discard('#files')
+        tags.add("#dirty")
+        kind = (sublime.KindId.VARIABLE, "d", "Type")
+
+    # Find results
+    if view.element() == "find_in_files:output":
+        tags.add("#search")
+        kind = (sublime.KindId.NAMESPACE, "?", "Namespace")
+
+    # Sheets with clones
+    if view.is_primary() is True and view.clones().__len__() > 0:
+        tags.add("#primary")
+        kind = (sublime.KindId.KEYWORD, "P", "Keyword")
+
+    # Sheet clones
+    if view.is_primary() is False and view.clones().__len__() > 0:
+        tags.add("#clones")
+        kind = (sublime.KindId.KEYWORD, "c", "Keyword")
+
+    return { "kind": kind, "tags": tags }
+
+def guess_sheet_name(sheet: sublime.Sheet):
+    name = "Untitled #%s" % sheet.id()
+    file_name = sheet.file_name()
+    view = sheet.view()
+
+    if file_name:
+        name = os.path.basename(file_name)
+    elif view is not None and view.name() != "":
+        name = view.name()
+
+    return name
+
+def replace_spaces_with_spaces(text):
+    match = re.match(r'^(\s+)', text)
+    if match:
+        spaces = match.group(1)
+        return re.sub(r'^\s+', ' ' * len(spaces), text)
+    else:
+        return text
+
+# Gets the visible lines surrounding the current line
+def get_visible_lines(view: sublime.View, visible_lines, view_selection):
+    fallback_line_string = None
+    line_strings: List[str] = []
+    MAX_SURROUND_LINE_THRESHOLD = 1
+
+    for line_index, line in enumerate(visible_lines):
+        line_string = view.substr(line).rstrip()
+
+        if fallback_line_string is None and line_string != "":
+            fallback_line_string = line_string
+
+        if line != view_selection:
+            continue
+
+        real_line_threshold = MAX_SURROUND_LINE_THRESHOLD + 1
+        lines = list(range(line_index-1, line_index-real_line_threshold, -1)) + [line_index] + list(range(line_index+1, line_index+real_line_threshold, 1))
+
+        for preview_line in lines:
+            if preview_line < 0 or preview_line >= len(visible_lines):
+                continue
+
+            line_string = view.substr(visible_lines[preview_line])
+            # @note does not work
+            # line_string = replace_spaces_with_spaces(line_string)
+
+            if line_string != "":
+                line_strings.append(line_string)
+
+        if line_strings.__len__() > 0:
+            return line_strings
+
+    return [fallback_line_string or ""]
+
+def generate_preview(view: sublime.View):
+    visible_lines = view.lines(view.visible_region())
+    view_selection = view.line(view.sel()[0].a)
+    preview = view.substr(view_selection).strip()
+
+    if preview.__len__() <= 0:
+        preview_lines = get_visible_lines(view, visible_lines, view_selection)
+        preview = preview_lines[0].lstrip() if preview_lines.__len__() > 0 and preview_lines[0].lstrip() != "" else preview
+
+    # @todo for groups, show the preview for the currently active in that group
+    return """<tt style='color:red'>%s</tt>""" % sublime.html.escape(preview)
 
 class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
-    ignore_highlight = False
+    def parseSheet(self, sheet: sublime.Sheet):
+        view = sheet.view()
+
+        if view is None or view.is_valid() is False:
+            return False
+
+        name = guess_sheet_name(sheet)
+        preview = generate_preview(view)
+        viewMeta = generate_view_meta(view)
+        kind = viewMeta['kind']
+        tags = viewMeta['tags']
+        file = sheet.file_name()
+
+        return { "name": name, "preview": preview, "kind": kind, "tags": tags, "file": file }
 
     def run(self, **kwargs):
         settings = plugin_settings()
         state = plugin_state()
 
-        ContextKeeperShowCommand.ignore_highlight = False
         self.highlighted_index = -1
         is_forward = kwargs.get('forward', True)
 
@@ -46,147 +162,65 @@ class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
         if is_forward is False:
             selected_index = stack_length - 1
 
+        postList = []
+
         for index,sheets in enumerate(stack_sheets):
             names = []
-            annotation = "#%s" % (index + 1)
+            files = []
             preview = ""
-            kind = sublime.KIND_FUNCTION
+            kind = None
             tags = set()
 
             for sheet in sheets:
-                view = sheet.view()
+                parsedSheet = self.parseSheet(sheet)
 
-                if view is None or view.is_valid() is False:
+                if parsedSheet is False:
                     stack.remove(sheet)
                     continue
 
-                name = "Untitled #%s" % sheet.id()
-                file_name = sheet.file_name()
+                names.append(parsedSheet['name'])
+                files.append(parsedSheet['file'])
+                tags = tags.union(parsedSheet['tags'])
 
-                if file_name:
-                    name = os.path.basename(file_name)
-                elif view.name() != "":
-                    name = view.name()
-
-                names.append(name)
-
-                # Get preview of current line
                 if preview == "":
-                    visible_lines = view.lines(view.visible_region())
-                    view_selection = view.line(view.sel()[0].a)
-                    preview = view.substr(view_selection).strip()
+                    preview = parsedSheet['preview']
 
-                    if preview.__len__() <= 0:
-                        preview_lines = self.get_visible_lines(view, visible_lines, view_selection)
-                        preview = preview_lines[0].lstrip() if preview_lines.__len__() > 0 and preview_lines[0].lstrip() != "" else preview
-
-                    # @todo for groups, show the preview for the currently active in that group
-                    preview = """<tt style='color:red'>%s</tt>""" % sublime.html.escape(preview)
-
-                # Set item type
-                # Normal sheet
-                if view.file_name() is not None and view.element() is None:
-                    tags.add("#files")
-
-                # Empty sheet
-                if view.file_name() is None and view.element() is None:
-                    tags.add("#empty")
-                    kind = (sublime.KindId.SNIPPET, "E", "Snippet")
-
-                # Dirty sheets
-                if  view.file_name() is not None and view.is_dirty():
-                    tags.add("#dirty")
-                    kind = (sublime.KindId.VARIABLE, "d", "Type")
-
-                # Find results
-                if view.element() == "find_in_files:output":
-                    tags.add("#search")
-                    kind = (sublime.KindId.NAMESPACE, "?", "Namespace")
-
-                # Sheets with clones
-                if view.is_primary() is True and view.clones().__len__() > 0:
-                    tags.add("#primary")
-                    kind = (sublime.KindId.KEYWORD, "P", "Keyword")
-
-                # Sheet clones
-                if view.is_primary() is False and view.clones().__len__() > 0:
-                    tags.add("#clones")
-                    kind = (sublime.KindId.KEYWORD, "c", "Keyword")
+                if kind is None:
+                    kind = parsedSheet['kind']
 
             if names.__len__() <= 0:
                 continue
 
-            trigger = ', '.join(names)
-
-            # @todo hacky tags for filter
-            # spaces = [' '] * 10
-            # spaces = ''.join(spaces)
-            # trigger = "%s%s%s" % (trigger, spaces, ' '.join(tags))
-            # annotation = ' '.join(tags)
-            annotation = ""
-
-            item = sublime.QuickPanelItem(trigger=trigger, annotation=annotation, kind=kind, details=preview)
+            trigger = ' + '.join(names)
+            joinedTags = ' '.join(tags)
+            item = sublime.QuickPanelItem(trigger=trigger, kind=kind, details=preview, annotation=joinedTags)
             items.append(item)
 
-        # @note planning to include existing files in the folder
-        # folders = self.window.folders()
-        # for folder in folders:
-        #     for root, dirs, files in os.walk(folder):
-        #         for file in files:
-        #             items.append(os.path.join(root, file))
+            file_label: str | None = files[0]
+
+            if file_label is not None:
+                open_folders = self.window.folders()
+
+                for folder in open_folders:
+                    file_label = file_label.replace("%s%s" % (folder, os.path.sep), "")
+
+            if file_label is None:
+                file_label = names[0]
+
+            expandedTrigger = "%s%s%s" % (joinedTags, ' | ', file_label)
+            item = sublime.QuickPanelItem(trigger=expandedTrigger, kind=kind, annotation=trigger)
+            postList.append(item)
 
         state["is_quick_panel_open"] = True
         state["highlighted_index"] = selected_index
 
         self.window.show_quick_panel(
-            items=items,
+            items=items + postList,
             selected_index=selected_index,
             flags=sublime.QuickPanelFlags.WANT_EVENT,
             on_select=lambda index, event=None: self.on_done(index, items, stack, initial_selection, event),
             on_highlight=lambda index: self.on_highlight(index, items, stack, initial_selection)
         )
-
-    def replace_spaces_with_spaces(self, text):
-        match = re.match(r'^(\s+)', text)
-        if match:
-            spaces = match.group(1)
-            return re.sub(r'^\s+', ' ' * len(spaces), text)
-        else:
-            return text
-
-    # Gets the visible lines surrounding the current line
-    def get_visible_lines(self, view: sublime.View, visible_lines, view_selection):
-        fallback_line_string = None
-        line_strings: List[str] = []
-        MAX_SURROUND_LINE_THRESHOLD = 1
-
-        for line_index, line in enumerate(visible_lines):
-            line_string = view.substr(line).rstrip()
-
-            if fallback_line_string is None and line_string != "":
-                fallback_line_string = line_string
-
-            if line != view_selection:
-                continue
-
-            real_line_threshold = MAX_SURROUND_LINE_THRESHOLD + 1
-            lines = list(range(line_index-1, line_index-real_line_threshold, -1)) + [line_index] + list(range(line_index+1, line_index+real_line_threshold, 1))
-
-            for preview_line in lines:
-                if preview_line < 0 or preview_line >= len(visible_lines):
-                    continue
-
-                line_string = view.substr(visible_lines[preview_line])
-                # @note does not work
-                # line_string = self.replace_spaces_with_spaces(line_string)
-
-                if line_string != "":
-                    line_strings.append(line_string)
-
-            if line_strings.__len__() > 0:
-                return line_strings
-
-        return [fallback_line_string or ""]
 
     def on_highlight(self, index, items, stack: ViewStack, selection):
         if index == -1:
@@ -231,9 +265,6 @@ class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
         if sheets is None:
             print("Sheets is missing!")
             # raise Exception("Sheets is missing!")
-            return
-
-        if ContextKeeperShowCommand.ignore_highlight is True:
             return
 
         self.window.select_sheets(sheets)
