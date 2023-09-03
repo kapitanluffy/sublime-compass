cd0b389de9004c4c2625a1e3bdc8bc34618fe2dc
kapitanluffy <kapitanluffy@gmail.com>
2023-09-04 00:13:30 +0800

    Add tags and unopened files

--- show_command.py
+++ show_command.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 import sublime
 import sublime_plugin
 from .utils import plugin_settings, plugin_state
@@ -8,6 +8,34 @@ import copy
 import os
 import re
 from pathlib import Path
+import subprocess
+
+def list_files(directory = "."):
+    settings = plugin_settings()
+    ripgrep = str(settings.get("ripgrep_path", ""))
+
+    if ripgrep == "" or os.path.exists(ripgrep) is False:
+        print("settings", ripgrep)
+        return None
+
+    command = [settings["ripgrep_path"], "--files", directory]
+
+    try:
+        # Run the command and capture the output
+        result = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,  # This makes sure the output is treated as text (str) rather than bytes
+            creationflags=subprocess.CREATE_NO_WINDOW,
+        )
+
+        if result.returncode != 0:
+            print(result.stderr)
+
+        return result.stdout.splitlines()
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def generate_view_meta(view: sublime.View):
     tags = set()
@@ -112,6 +140,20 @@ def generate_preview(view: sublime.View):
     # @todo for groups, show the preview for the currently active in that group
     return """<tt style='color:red'>%s</tt>""" % sublime.html.escape(preview)
 
+class File():
+    def __init__(self, file, folder):
+        self.file = file
+        self.folder = folder
+        trimmedRoot = file.replace("%s\\" % folder, "")
+        basename = os.path.basename(file)
+        self.relative = "%s\\%s" % (trimmedRoot, basename)
+
+    def get_file_name(self):
+        return self.relative
+
+    def get_full_path(self):
+        return self.file
+
 class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
     def parseSheet(self, sheet: sublime.Sheet):
         view = sheet.view()
@@ -127,6 +169,21 @@ class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
         file = sheet.file_name()
 
         return { "name": name, "preview": preview, "kind": kind, "tags": tags, "file": file }
+
+    def parse_listed_files(self):
+        folders = self.window.folders()
+        items: List[File] = []
+
+        for folder in folders:
+            files = list_files(folder)
+
+            if files is None:
+                return None
+
+            for file in files:
+                items.append(File(file, folder))
+
+            return items
 
     def run(self, **kwargs):
         settings = plugin_settings()
@@ -145,7 +202,7 @@ class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
         if self.window.views_in_group(group).__len__() <= 0:
             return
 
-        items = []
+        items: List[sublime.QuickPanelItem] = []
         current_view = self.window.active_view_in_group(group)
 
         if current_view is None:
@@ -154,7 +211,8 @@ class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
         initial_selection = self.window.selected_sheets_in_group(group)
         stack_length = len(stack.all())
         selected_index = 0
-        stack_sheets = copy.deepcopy(stack.all())
+        # stack_sheets = copy.deepcopy(stack.all())
+        stack_sheets = stack.all()
 
         if settings["jump_to_most_recent_on_show"] is True:
             selected_index = 1
@@ -162,8 +220,8 @@ class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
         if is_forward is False:
             selected_index = stack_length - 1
 
-        post_list = []
-        items_meta: List[SheetGroup] = []
+        post_list: List[sublime.QuickPanelItem] = []
+        items_meta: List[Union[SheetGroup, File]] = []
         post_list_meta: List[SheetGroup] = []
 
         for index, sheets in enumerate(stack_sheets):
@@ -193,8 +251,9 @@ class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
             if names.__len__() <= 0:
                 continue
 
+            is_tags_enabled = settings.get("enable_tags")
             trigger = ' + '.join(names)
-            joinedTags = ' '.join(tags)
+            joinedTags = ' '.join(tags) if is_tags_enabled else ''
             item = sublime.QuickPanelItem(trigger=trigger, kind=kind, details=preview, annotation=joinedTags)
             items.append(item)
             items_meta.append(sheets)
@@ -210,36 +269,48 @@ class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
             if file_label is None:
                 file_label = names[0]
 
-            expandedTrigger = "%s%s%s" % (joinedTags, ' | ', file_label)
-            item = sublime.QuickPanelItem(trigger=expandedTrigger, kind=kind, annotation=trigger)
-            post_list.append(item)
-            post_list_meta.append(sheets)
+            if is_tags_enabled is True:
+                expandedTrigger = "%s%s%s" % (joinedTags, ' | ', file_label)
+                item = sublime.QuickPanelItem(trigger=expandedTrigger, kind=kind, annotation=trigger)
+                post_list.append(item)
+                post_list_meta.append(sheets)
+
+        unopened_files = self.parse_listed_files()
+        unopened_files_items: List[sublime.QuickPanelItem] = []
+        unopened_files_meta = []
+
+        if unopened_files is not None:
+            for file in unopened_files:
+                filename = file.get_file_name()
+                trigger = "#open > %s" % (filename)
+                item = sublime.QuickPanelItem(trigger=trigger, kind=(sublime.KindId.VARIABLE, "f", "Function"))
+                unopened_files_items.append(item)
+                unopened_files_meta.append(file)
 
         state["is_quick_panel_open"] = True
         state["highlighted_index"] = selected_index
 
-        items = items + post_list
-        items_meta = items_meta + post_list_meta
+        items = items + post_list + unopened_files_items
+        items_meta = items_meta + post_list_meta + unopened_files_meta
 
         self.window.show_quick_panel(
             items=items,
             selected_index=selected_index,
-            flags=sublime.QuickPanelFlags.WANT_EVENT,
             on_select=lambda index: self.on_done(index, items, stack, initial_selection, items_meta),
             on_highlight=lambda index: self.on_highlight(index, items, stack, initial_selection, items_meta)
         )
 
-    def on_highlight(self, index, items, stack: ViewStack, selection, items_meta):
+    def on_highlight(self, index: int, items, stack: ViewStack, selection, items_meta: List[Union[SheetGroup, File]]):
         if index == -1:
             raise Exception("Cannot highlight index: -1")
 
         sheets = items_meta[index]
+        self.highlighted_index = index
 
-        if sheets is None and (len(stack.all()) <= index < len(items)):
-            self.window.open_file(items[index], sublime.TRANSIENT)
+        if isinstance(sheets, File) and sheets is not None:
+            self.window.open_file(sheets.get_full_path(), sublime.TRANSIENT)
 
-        if sheets is not None:
-            self.highlighted_index = index
+        if isinstance(sheets, SheetGroup) and sheets is not None:
             self.window.select_sheets(sheets)
 
         state = plugin_state()
@@ -247,7 +318,7 @@ class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
 
         ContextKeeperShowCommand.ignore_highlight=False
 
-    def on_done(self, index, items, stack: ViewStack, selection, items_meta):
+    def on_done(self, index, items, stack: ViewStack, selection, items_meta: List[Union[SheetGroup, File]]):
         state = plugin_state()
 
         if index == -1:
@@ -261,18 +332,15 @@ class ContextKeeperShowCommand(sublime_plugin.WindowCommand):
 
         state["is_quick_panel_open"] = False
 
-        if sheets is None and (len(stack.all()) <= index < len(items)):
-            print("selected transient!", items[index])
-            # self.window.open_file(items[index])
+        if isinstance(sheets, File) and sheets is not None:
+            self.window.open_file(sheets.get_full_path())
             return
 
-        if sheets is None:
-            print("Sheets is missing!")
+        if isinstance(sheets, SheetGroup) and sheets is not None:
+            self.window.select_sheets(sheets)
+
+            # refocus on the selected sheet
+            focused = sheets.get_focused()
+            if len(sheets) > 0 and focused is not None:
+                self.window.focus_sheet(focused)
             return
-
-        self.window.select_sheets(sheets)
-
-        # refocus on the selected sheet
-        focused = sheets.get_focused()
-        if focused is not None:
-            self.window.focus_sheet(focused)
