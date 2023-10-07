d267b308a5f730ff799e09a3a6fced650499f93c
kapitanluffy <kapitanluffy@gmail.com>
2023-10-07 13:46:49 +0800

    Improve dump stack command

--- src/commands/dump_stack.py
+++ src/commands/dump_stack.py
@@ -1,7 +1,16 @@
 from ..stack import STACK
+import sublime
 import sublime_plugin
 
+
 class CompassDumpStackCommand(sublime_plugin.WindowCommand):
-    def run(self, **kwargs):
+    def run(self):
         window_stack = list(filter(lambda block: block[0] == self.window.id(), STACK))
-        print("STACK>", window_stack, len(window_stack))
+        print("STACK>")
+        for item in window_stack:
+            for sheet in item[2]:
+                s = sublime.Sheet(sheet)
+                v = s.view()
+                name = v.name() if v is not None else None
+                print("\t%s: %s" % (s.id(), s.file_name() or name or "Untitled #%s" % s.id()))
+        print("ENDSTACK>")
