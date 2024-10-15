bc4b36d0f44006b38ea30151ee14eb298676c8c1
kapitanluffy <485799+kapitanluffy@users.noreply.github.com>
2024-10-16 02:47:48 +0800

    Add reindex command

--- src/commands/index_files.py
+++ src/commands/index_files.py
@@ -1,14 +1,12 @@
-import time
-from ..utils import generate_files
-from ..stack import STACK
+from ..core import load
 import sublime
 import sublime_plugin
 
 class CompassIndexFilesCommand(sublime_plugin.WindowCommand):
     def run_index(self):
-        print("indexing!")
-        for file in generate_files(self.window):
-            sublime.set_timeout_async(lambda f=file: print(f.get_file_name()))
+        print("Reindexing files...")
+        self.window.run_command('compass_clear_cache')
+        load()
 
     def run(self, **kwargs):
         sublime.set_timeout_async(self.run_index)
